36bdc8237892e28fa868416f7a27482df3ebac73
preston.thiele <prestonthiele@uchicago.edu>
2022-07-14 14:35:46 -0500

    idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/header';
+import About from './components/Body/about';
 //import Grid from '@material-ui/core/Grid';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     <h1 className='comingSoon'>Coming Soon...</h1>
     </div>
   );
---
+++ src/components/Body/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './about.css';
+import { IconButton } from '@material-ui/core';
+const About = () => (
+  <div className="about">
+    <h1>About Me</h1>
+  </div>
+)
+export default About;
